df4e4c6c0a22fabbf7314d8d1b90a68fdf319b97
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-11-23 22:31:48 +0700

    get all attendances for HR admin

--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 router.get('/', adminController.index);
+router.get('/attendances', adminController.getAllAttendances);
 
 module.exports = router;
