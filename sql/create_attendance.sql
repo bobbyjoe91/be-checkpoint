7810864c373a0721c82685e6899c662b85433d5d
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-11-24 19:32:51 +0700

    change employee and attendance schema

--- sql/create_attendance.sql
+++ sql/create_attendance.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Attendance (
   attendance_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  employee_id INT DEFAULT NULL,
   date DATE NOT NULL,
   time_in TIME NOT NULL,
-  time_out TIME NOT NULL
+  time_out TIME DEFAULT NULL
 );
--- sql/create_employee.sql
+++ sql/create_employee.sql
@@ -1,5 +1,8 @@
 CREATE TABLE Employee (
   employee_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  position_id INT,
+  division_id INT,
+  attendance_id INT,
   name VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   phone_number VARCHAR(20) NOT NULL,
@@ -7,15 +10,10 @@ CREATE TABLE Employee (
   password CHAR(128) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  position_id INT,
-  division_id INT,
-  attendance_id INT,
   FOREIGN KEY (position_id) REFERENCES Positions(position_id),
   FOREIGN KEY (division_id) REFERENCES Division(division_id),
-  FOREIGN KEY (attendance_id) REFERENCES Attendance(attendance_id)
+  FOREIGN KEY (attendance_id) REFERENCES Attendance(attendance_id) ON DELETE SET NULL
 );
-
-ALTER TABLE Attendance ADD employee_id INT;
 
 ALTER TABLE Attendance ADD
 CONSTRAINT fk_employee_id
