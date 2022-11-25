85506ed8351cfc57c371b0b9997fc2d8ed97ee07
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-11-25 23:26:10 +0700

    add cors to allow front-end requests

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 const port = 8000;
 
+app.use(cors({
+  origin: ['http://localhost:3000']
+}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
